90f71d99abf654df3af5f1076fdb2f4d29be01fb
GeorgiaSilva <georgiacarin@gmail.com>
2026-03-22 15:12:39 -0300

    feat: implement login and registration functionality and testing login request

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+
+import { loginSchema, type LoginData } from '../schemas/auth.schema'
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { login } from '../services/auth.service';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
+  const navgate = useNavigate();
+  const { register, handleSubmit } = useForm<LoginData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: LoginData) => {
+    try {
+      await login(data.email, data.password);
+      navgate("/");
+    } catch (error) {
+      console.error("Login failed", error);
+    }
+  };
+
   return (
-    <div>LoginPage</div>
+    <main>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="email" placeholder="E-mail" {...register("email")} />
+        <input type="password" placeholder="Senha" {...register("password")} />
+        <button type="submit">Entrar</button>
+      </form>
+    </main>
   )
 }
---
+++ src/services/auth.service.ts
@@ -0,0 +1,72 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+if (!API_URL) {
+  throw new Error("API_URL is not defined in environment variables");
+}
+const authService = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const saveToken = (token: string) => {
+  localStorage.setItem("token", token);
+};
+
+export const getToken = () => {
+  return localStorage.getItem("token");
+};
+
+export const login = async (email: string, password: string) => {
+  try {
+    const response = await authService.post("/auth/login", {
+      email,
+      password,
+    });
+    saveToken(response.data.token);
+    localStorage.setItem("user", JSON.stringify(response.data.user));
+    return response.data;
+  } catch (error) {
+    throw new Error("Login failed");
+  }
+};
+
+export const register = async (
+  name: string,
+  email: string,
+  password: string,
+) => {
+  try {
+    const response = await authService.post("/auth/register", {
+      name,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Registration failed");
+  }
+};
+
+export const logout = async () => {
+  const token = getToken();
+  if (token) {
+    try {
+      await authService.post(
+        "/auth/logout",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  }
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+};
