cbbbfbed37bae4181b52e532825ac5e4d51f8809
GeorgiaSilva <georgiacarin@gmail.com>
2026-03-22 22:28:47 -0300

    style: update ThemeToggle button positioning and adjust dark theme background colors

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -6,11 +6,10 @@ export const ThemeToggle = () => {
 
   return (
     <button
-
       onClick={toggleTheme}
-      className="fixed right-4 top-4 text-slate-800 dark:text-slate-100 px-4 py-2 rounded"
+      className=" text-slate-800 dark:text-slate-100 px-4 py-2 rounded"
     >
       {theme === "dark" ? <Sun size={18} /> : <Moon size={18} />}
     </button>
-  )
-}
+  );
+};
